398a2953e434a9ced1f7451db7f2fffa96b315f9
KokosMjolkeKua <insmi6764@oslomet.no>
2025-09-26 16:27:48 +0200

    kommiy

---
+++ src/Arithmetic_Logical_Methods_Assignments/Assignment_6.java
@@ -0,0 +1,10 @@
+package Arithmetic_Logical_Methods_Assignments;
+import static javax.swing.JOptionPane.*;
+public class Assignment_6 {
+    public static void main(String[]args){
+        String inputUSD = showInputDialog("How much USD do you have?");
+
+        double USD;
+
+    }
+}
---
+++ src/Variables_Assignmnets/Assignment_1.java
@@ -0,0 +1,17 @@
+package Variables_Assignmnets;
+import static javax.swing.JOptionPane.*;
+public class Assignment_1 {
+    public static void main(String[]args){
+        String firstName;
+        String lastName;
+
+        showMessageDialog(null, "Hi and Welcome!");
+
+        firstName = showInputDialog("What is your first-name?");
+        lastName = showInputDialog("What is your last-name?");
+
+        showMessageDialog(null, "Welcome " + firstName + " " + lastName + " to our Program!");
+
+        showMessageDialog(null, "Goodbye!");
+    }
+}

101f7cd45c9d89d1e65eb125c5bbcb47fadac70d
KokosMjolkeKua <insmi6764@oslomet.no>
2025-10-08 16:24:11 +0200

    kommiy

---
+++ src/Control_Structure/Assignment_5_B.java
@@ -0,0 +1,11 @@
+package Control_Structure;
+import static javax.swing.JOptionPane.*;
+public class Assignment_5_B {
+    public static void main(String[]args){
+    String month = showInputDialog(null, "What is the current month?");
+
+    switch (month){
+
+    }
+    }
+}
